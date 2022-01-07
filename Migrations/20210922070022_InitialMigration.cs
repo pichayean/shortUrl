809996693bf7dd54b053f2b1fbc508d77092117a
pichayean <pichayeanyensiri@gmail.com>
2022-01-07 09:59:14 +0700

    change database

---
+++ Migrations/20210922070022_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShortUrl.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "short_url");
+
+            migrationBuilder.CreateTable(
+                name: "Url",
+                schema: "short_url",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "character(50)", fixedLength: true, maxLength: 50, nullable: false),
+                    OriginalURL = table.Column<string>(type: "character(1024)", fixedLength: true, maxLength: 1024, nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp", nullable: false, defaultValueSql: "(NOW())")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Url", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Url",
+                schema: "short_url");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -77,7 +77,7 @@ namespace ShortUrl
 }
 
 // https://www.npgsql.org/efcore/
-// dotnet ef dbcontext scaffold "Host=194.233.73.42;Database=short_url;Username=postgres;Password=Ld4t5555" Npgsql.EntityFrameworkCore.PostgreSQL
+// dotnet ef dbcontext scaffold "Host=144.126.140.118;Database=short_url;Username=postgres;Password=Ld4t5555" Npgsql.EntityFrameworkCore.PostgreSQL
 
     // <PackageReference Include="Npgsql.EntityFrameworkCore.PostgreSQL" Version="5.0.7" />
     
